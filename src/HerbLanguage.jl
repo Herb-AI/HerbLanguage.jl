c39faa37c87d625beefef897a28cd55091cf2cf9
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2021-04-09 16:33:34 +0200

    initiated Pkg

--- src/HerbLanguage.jl
+++ src/HerbLanguage.jl
@@ -1,14 +1,14 @@
 module HerbLanguage
 
-# push!(LOAD_PATH, pwd())
-
 include("structs.jl")
 include("context.jl")
 include("parse.jl")
 include("operators.jl")
+include("utils.jl")
 
 export Type, thing, Constant, Variable, Functor, Structure, List, LPair, Predicate, Literal, Negation, Conj, Clause, Disjunction, Recursion, Program
 export Context, c_const!, c_var!, c_functor!, c_pred!, c_type!
-export parse_prolog, split_compound_args
+export parse_prolog, split_compound_arg
+export is_ground, get_vars
 
-end
+end # module
